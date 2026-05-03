f090797de2f0de3ac740f33794dc0fbb7a4d1e96
Harshit Saxena <harshit214saxena@gmail.com>
2026-05-04 00:40:14 +0530

    fix: update authentication logic in proxy middleware to ensure user protection on routes

--- frontend/src/proxy.ts
+++ frontend/src/proxy.ts
@@ -1,22 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/",
   "/p(.*)",
-  "/code-review" 
+  "/code-review",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect();
+  const { userId } = await auth();
+
+  if (isProtectedRoute(req) && !userId) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|webmanifest)).*)",
     "/(api|trpc)(.*)",
   ],
 };
