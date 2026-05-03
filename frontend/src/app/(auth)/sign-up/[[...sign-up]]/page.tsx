8d2817bf0a7603241451a395e64055fc802b0720
Harshit Saxena <harshit214saxena@gmail.com>
2026-05-03 22:40:53 +0530

    fix: remove afterSignUpUrl prop from SignUp component

--- frontend/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ frontend/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -5,7 +5,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <SignUp afterSignUpUrl="/" />
+      <SignUp />
     </div>
   );
 }
